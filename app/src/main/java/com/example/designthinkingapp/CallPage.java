485b9abd0966eeaf8f8603bbc23d05c9bb6dacc4
Cooper <Cooper.plath@my.jcu.edu.au>
2022-09-15 16:36:17 +1000

    ContactPage Completed with intents passing through contactdetails

---
+++ app/src/main/java/com/example/designthinkingapp/CallPage.java
@@ -0,0 +1,14 @@
+package com.example.designthinkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_page);
+    }
+}

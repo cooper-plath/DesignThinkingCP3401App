45879a5984f7ce8556124bdee874d87e876648bd
giftos1 <69237210+giftos1@users.noreply.github.com>
2022-09-20 15:37:34 +1000

    Google Java Class commit

---
+++ app/src/main/java/com/example/designthinkingapp/GoogleActivity.java
@@ -0,0 +1,13 @@
+package au.edu.jcu.google;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GoogleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google);
+    }
+}
